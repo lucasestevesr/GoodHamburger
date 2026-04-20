30f6b4523e6adabac4702da9e70db1b52d5f8388
Lucas E. R. <56853712+lucasestevesr@users.noreply.github.com>
2026-04-19 22:30:38 -0300

    feat: adicionando testes unitários do dominio

---
+++ tests/GoodHamburger.Tests/Mocks/ProductMock.cs
@@ -0,0 +1,36 @@
+﻿using GoodHamburger.Domain.Entities.Products;
+namespace GoodHamburger.Tests.Unit.Mocks
+{
+    public static class ProductMock
+    {
+        public static Product CreateBurger() => new()
+        {
+            Name = "X Burger",
+            Price = 5.00m,
+            Category = ProductCategory.Burger,
+            IsActive = true
+        };
+        public static Product CreateEggBurger() => new()
+        {
+            Name = "X Egg",
+            Price = 4.50m,
+            Category = ProductCategory.Burger,
+            IsActive = true
+        };
+
+        public static Product CreateSide() => new()
+        {
+            Name = "Batata frita",
+            Price = 2.00m,
+            Category = ProductCategory.Side,
+            IsActive = true
+        };
+        public static Product CreateDrink() => new()
+        {
+            Name = "Refrigerante",
+            Price = 2.50m,
+            Category = ProductCategory.Drink,
+            IsActive = true
+        };
+    }
+}
